73c8984745b43cf03913353250001095feeb5fe5
Corentin <corentin.huibant@laposte.net>
2017-01-29 15:46:55 +0100

    Le cinquième exercice : fonction qui prend en paramètre deux chaînes de caractère et qui retourne la fusion de ces deux chaînes.

---
+++ exercice5.php
@@ -0,0 +1,8 @@
+<?php
+$norm = "Corentin";
+$surName = "HUIBANT";
+function name($norm, $surName) {
+  return "Je suis " . $norm . "" . $surName . "";
+}
+// echo "Je suis " . $norm . "" . $surName . "";
+ ?>
